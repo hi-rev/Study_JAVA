70ece8fa2dfdc6a86b481a09b703b046bbbde012
hi-rev <hirev05@gmail.>
2022-07-19 13:36:36 +0900

    데이터타입과 연산

---
+++ src/Datatype.java
@@ -0,0 +1,11 @@
+public class Datatype {
+    public static void main(String[] args) {
+        System.out.println(6);  // Number
+        System.out.println("six");  // String
+        System.out.println("6");  // Str
+        System.out.println(6+6);  // Num
+        System.out.println("6" + "6");  // Str
+        System.out.println(6 * 6);  // Num
+        System.out.println("1234".length());  // 문자열의 길이
+    }
+}
---
+++ src/StringApp.java
@@ -0,0 +1,13 @@
+public class StringApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");  // String
+        System.out.println('H');  // Character
+        // -> Char 은 쓸일이 잘 없으므로 대부분 Str 을 쓴다.
+        // ''는 문자, ""는 문자열
+
+        // \n = new line
+        System.out.println("Hello\nWorld");
+        // Hello "World"
+        System.out.println("Hello \"World\"");
+    }
+}
