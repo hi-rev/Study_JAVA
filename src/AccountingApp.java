1fd13142631bdac1f02c49bd0ec592707d13e5d7
NaGyeong <hirev05@gmail.com>
2022-07-25 21:44:57 +0900

    JAVA - 제어문 마무리

--- src/AccountingApp.java
+++ src/AccountingApp.java
@@ -1,36 +1,25 @@
 public class AccountingApp {
     public static void main(String[] args) {
-        double valueOfSupply = 15000.0;  // 공급가액
-        double vatRate = 0.1;  // 세금 비율
-        double expenseRate = 0.3;  // 비용 비율
-        double vat = valueOfSupply * vatRate;  // 세금
-        double total = valueOfSupply + vat;  // 판매가액
-        double expense = valueOfSupply * expenseRate;  // 비용
-        double income = valueOfSupply - expense;  // 수익
 
-        System.out.println("Value Of Supply: " + valueOfSupply);
-        System.out.println("VAT: " + vat);
-        System.out.println("Total: " + total);
-        System.out.println("Expense: " + expense);
-        System.out.println("Income: " + income);
+        double valueOfSupply = 10000.0;
+        double vatRate = 0.1;
+        double expenseRate = 0.3;
+        double vat = valueOfSupply * vatRate;
+        double total = valueOfSupply + vat;
+        double expense = valueOfSupply * expenseRate;
+        double income = valueOfSupply - expense;
+        double dividend1 = income * 0.5;
+        double dividend2 = income * 0.3;
+        double dividend3 = income * 0.2;
 
-        double[] dividendRates = new double[3];
-        dividendRates[0] = 0.5;
-        dividendRates[1] = 0.3;
-        dividendRates[2] = 0.2;
+        System.out.println("Value of supply: " + valueOfSupply);  // 공급가
+        System.out.println("VAT: " + vat);  // 부가가치세
+        System.out.println("Total: " + total);  // 가격
+        System.out.println("Expense: " + expense);  // 비용
+        System.out.println("Income: " + income);  // 수입
+        System.out.println("Dividend 1: " + dividend1);  // 배당1
+        System.out.println("Dividend 2: " + dividend2);  // 배당2
+        System.out.println("Dividend 3: " + dividend3);  // 배당3
 
-        // 수익이 10000원 이하이면 1번이 모두 가지고,
-        // 수익이 10000원 초과이면 5:3:2 비율로 나눈다.
-        if (income > 10000.0) {
-            int i = 0;
-            while (i < dividendRates.length) {
-                System.out.println("Dividend " + (i+1) + ": " + (income * dividendRates[i]));
-                i += 1;
-            }
-        } else {
-            System.out.println("Dividend 1: " + income);
-            System.out.println("Dividend 2: " + 0.0);
-            System.out.println("Dividend 3: " + 0.0);
-        }
     }
 }
---
+++ src/AuthApp3.java
@@ -0,0 +1,29 @@
+public class AuthApp3 {
+    public static void main(String[] args) {
+        // 유저 배열 생성
+        // String[] users = {"nana", "haha", "lili"};
+        String[][] users = {
+                {"nana", "1111"},
+                {"haha", "2222"},
+                {"lili", "3333"}
+        };
+        String inputId = args[0];
+        String inputPass = args[1];
+
+        boolean isLogined = false;
+        for (int i = 0; i < users.length; i++) {
+            String currentId = users[i][0];
+            String currentPass = users[i][1];
+            if (currentId.equals(inputId) && (currentPass.equals(inputPass))) {
+                isLogined = true;
+                break;
+            }
+        }
+        System.out.println("Hi, ");
+        if (isLogined) {
+            System.out.println("Master!!");
+        } else {
+            System.out.println("Who are you?");
+        }
+    }
+}
