b432af64f98973b10f422290507f8c0ad8ea49cd
hi-rev <hirev05@gmail.com>
2022-08-05 15:40:16 +0900

    Algorithm Practice

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[9];
-        for (int i = 0; i < 9; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        int a = arr[0];
-        int idx = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (a < arr[i]) {
-                a = arr[i];
-                idx = i;
+        int c = sc.nextInt();  // test case
+        for (int i = 0; i < c; i++) {
+            int n = sc.nextInt();  // 학생의 수
+            int[] std = new int[n];
+            int sum = 0;
+            for (int j = 0; j < n; j++) {  // 학생 점수 입력
+                std[j] = sc.nextInt();
+                sum += std[j];  // 총점
             }
+            int mean = sum / n;  // 평균값
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (std[j] > mean) {
+                    cnt++;
+                }
+            }
+            System.out.printf("%.3f", (double)cnt/n * 100);
+            System.out.println("%");
         }
-        System.out.println(a);
-        System.out.println(idx + 1);
     }
 }
 
